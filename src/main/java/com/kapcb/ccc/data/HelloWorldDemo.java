23f0a8888abb564861faa9f9cd3e3e4b35f65549
Eirccc <eircccallroot@yeah.net>
2020-05-29 20:41:58 +0800

    基本数据类型

---
+++ src/main/java/com/kapcb/ccc/data/HelloWorldDemo.java
@@ -0,0 +1,21 @@
+package com.kapcb.ccc.data;
+
+/**
+ * <a>Title:HelloWorldDemo</a>
+ * <a>Author：ccc<a>
+ * <a>Description：<a>
+ *
+ * @author ccc
+ * @version 1.0.0
+ * @date 2020/5/29 20:31
+ */
+public class HelloWorldDemo {
+
+    public static void main(String[] args) {
+        byte a = 40;
+        byte b = 50;
+
+        int x = a + b;
+        System.out.println("x = " + x);
+    }
+}
