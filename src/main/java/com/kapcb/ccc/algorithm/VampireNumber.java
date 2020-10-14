f6231dffef634f86e9fb445311f1c1e06027d0a0
Eirccc <eircccallroot@yeah.net>
2020-10-14 23:40:11 +0800

    VampireNumber

--- src/main/java/com/kapcb/ccc/algorithm/VampireNumber.java
+++ src/main/java/com/kapcb/ccc/algorithm/VampireNumber.java
@@ -2,20 +2,17 @@ package com.kapcb.ccc.algorithm;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 /**
  * <a>Title:VampireNumber</a>
  * <a>Author：ccc<a>
- * <a>Description：<a>
+ * <a>Description：吸血鬼数<a>
  *
  * @author ccc
  * @version 1.0.0
  * @date 2020/10/14 22:01
  */
 public class VampireNumber {
-
-//    Logger logger = Logger.getLogger(VampireNumber.class.getName());
 
     public static void main(String[] args) throws Exception {
         List<String> vampireNumber = getVampireNumber(4);
@@ -24,7 +21,7 @@ public class VampireNumber {
 
     private static List<String> getVampireNumber(int target) throws Exception {
         if (target <= 0) {
-            throw new Exception("输入参数有误");
+            throw new Exception("您输入的参数有误");
         }
         List<String> elementList = new ArrayList<>();
         loop1:
@@ -34,37 +31,27 @@ public class VampireNumber {
                 boolean flag = true;
                 int result = i * j;
                 String targetNumber = result + "";
-                System.out.println("targetNumber = " + targetNumber);
                 if (targetNumber.length() < 4) {
                     flag = false;
-                    System.out.println("j = " + j);
                     continue loop2;
                 }
                 if (flag) {
                     String elementOne = i + "";
-                    System.out.println("elementOne = " + elementOne);
                     String elementTwo = j + "";
-                    System.out.println("elementTwo = " + elementTwo);
                     String element = elementOne.concat(elementTwo);
-                    System.out.println("element = " + element);
                     String[] split = element.split("");
                     String tempString = targetNumber;
-                    System.out.println("split.length = " + split.length);
                     for (int k = 0; k < split.length; k++) {
-                        System.out.println("k = " + k);
                         if (!tempString.contains(split[k])) {
-                            continue ;
+                            continue loop2;
                         } else {
-//                            int startIndex = tempString.indexOf(split[k]);
-//                            System.out.println("startIndex = " + startIndex);
-//                            tempString = targetNumber.substring(startIndex, startIndex + 1);
-                            tempString = targetNumber.replaceFirst(split[k], "");
-                            System.out.println("split[k] = " + split[k]);
-                            System.out.println("tempString = " + tempString);
+                            tempString = tempString.replaceFirst(split[k], "");
                         }
                     }
-                    String s = targetNumber + "=" + elementOne + "*" + elementTwo;
-                    elementList.add(s);
+                    if (tempString.isEmpty()) {
+                        String s = targetNumber + "=" + elementOne + "*" + elementTwo;
+                        elementList.add(s);
+                    }
                 }
             }
         }
