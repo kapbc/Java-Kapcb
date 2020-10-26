7f4408c64b02e598b81e3499c6cd7e5a01e03335
kapcb <55942617+Eircc@users.noreply.github.com>
2020-10-26 11:24:36 +0800

    Create Art.java

---
+++ src/main/java/com/kapcb/ccc/Art.java
@@ -0,0 +1,13 @@
+/**
+ * @author kapcb
+ * @version 1.0.0
+ * @classname Art
+ * @description TODO
+ * @date 2020/10/26 11:20
+ */
+public class Art {
+
+    public Art(){
+        System.out.println("Art constructor");
+    }
+}
