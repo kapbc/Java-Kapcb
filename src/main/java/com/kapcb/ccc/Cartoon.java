8d8d8181bf32fdb332b1bd2e9966989433104093
kapcb <55942617+Eircc@users.noreply.github.com>
2020-10-26 11:25:24 +0800

    Create Cartoon.java

---
+++ src/main/java/com/kapcb/ccc/Cartoon.java
@@ -0,0 +1,17 @@
+/**
+ * @author kapcb
+ * @version 1.0.0
+ * @classname Cartoon
+ * @description TODO
+ * @date 2020/10/26 11:22
+ */
+public class Cartoon extends Drawing{
+
+//    public Cartoon(){
+//        System.out.println("Cartoon constructor");
+//    }
+
+    public static void main(String[] args) {
+        Cartoon cartoon = new Cartoon();
+    }
+}
