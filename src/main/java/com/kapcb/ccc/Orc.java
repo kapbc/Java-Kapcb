39c300d936756ab30f3fecfdf4ffc1b7a3c38b48
kapcb <55942617+kapbc@users.noreply.github.com>
2020-10-26 16:52:25 +0800

    extend

---
+++ src/main/java/com/kapcb/ccc/Orc.java
@@ -0,0 +1,25 @@
+public class Orc extends Villain {
+    private int orcNumber;
+
+    public Orc(String name, int orcNumber) {
+        super(name);
+        this.orcNumber = orcNumber;
+    }
+
+    public void change(String name, int orcNumber) {
+        set(name);
+        this.orcNumber = orcNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Orc " + orcNumber + ": " + super.toString();
+    }
+
+    public static void main(String[] args) {
+        Orc limburger = new Orc("Limburger", 12);
+        System.out.println(limburger);
+        limburger.change("Bob", 19);
+        System.out.println(limburger);
+    }
+}
