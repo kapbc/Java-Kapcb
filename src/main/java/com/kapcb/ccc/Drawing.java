c524b51ab4533a5cebd41edd4ee0b124f0a40b70
kapcb <55942617+Eircc@users.noreply.github.com>
2020-10-26 11:25:05 +0800

    Create Drawing.java

---
+++ src/main/java/com/kapcb/ccc/Drawing.java
@@ -0,0 +1,13 @@
+/**
+ * @author kapcb
+ * @version 1.0.0
+ * @classname Drawing
+ * @description TODO
+ * @date 2020/10/26 11:21
+ */
+public class Drawing extends Art{
+
+    public Drawing(){
+        System.out.println("Drawing constructor");
+    }
+}
