7ca4281631983836499acb96d4e21317dd4e52d8
kapcb <55942617+kapbc@users.noreply.github.com>
2020-10-27 14:16:56 +0800

    Dinosaur

---
+++ src/main/java/com/kapcb/ccc/practice/think/Dinosaur.java
@@ -0,0 +1,18 @@
+package com.kapcb.ccc.practice.think;
+
+/**
+ * @author kapcb
+ * @version 1.0.0
+ * @classname Dinosaur
+ * @date 2020/10/27 14:09
+ */
+public final class Dinosaur {
+
+    int i = 7;
+    int j = 1;
+
+    SmallBrain smallBrain = new SmallBrain();
+
+    public void f(){
+    }
+}
