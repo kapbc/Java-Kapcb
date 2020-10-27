2f728613b4f6a077497f25415c6883b59d55c4a3
kapcb <55942617+kapbc@users.noreply.github.com>
2020-10-27 11:41:35 +0800

    Value

---
+++ src/main/java/com/kapcb/ccc/practice/think/Value.java
@@ -0,0 +1,18 @@
+package com.kapcb.ccc.practice.think;
+
+/**
+ * @author kapcb
+ * @version 1.0.0
+ * @classname Value
+ * @date 2020/10/27 11:00
+ */
+public class Value {
+    int i;
+
+    public Value() {
+    }
+
+    public Value(int i) {
+        this.i = i;
+    }
+}
