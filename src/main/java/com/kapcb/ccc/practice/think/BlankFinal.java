e644c99ac8e082580c2fa614aac95d874f17fc6b
kapcb <55942617+kapbc@users.noreply.github.com>
2020-10-27 13:25:47 +0800

    BlankFinal

---
+++ src/main/java/com/kapcb/ccc/practice/think/BlankFinal.java
@@ -0,0 +1,28 @@
+package com.kapcb.ccc.practice.think;
+
+/**
+ * @author kapcb
+ * @version 1.0.0
+ * @classname BlankFinal
+ * @date 2020/10/27 13:14
+ */
+public class BlankFinal {
+    private final int i = 0;
+    private final int j; //Blank final
+    private final Poppet poppet; //Must be initialized in the constructor
+
+    public BlankFinal() {
+        this.j = 1; // initialized blank final reference
+        this.poppet = new Poppet(1); // initialized blank final reference
+    }
+
+    public BlankFinal(int x) {
+        j = x; // initialized blank final reference
+        poppet = new Poppet(x); // initialized blank final reference
+    }
+
+    public static void main(String[] args) {
+        new BlankFinal(50);
+        new BlankFinal(50);
+    }
+}
