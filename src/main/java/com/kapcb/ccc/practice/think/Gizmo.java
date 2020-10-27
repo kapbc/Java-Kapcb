ddc22a738156e585ac249fc529d5b7c096d5941a
kapcb <55942617+kapbc@users.noreply.github.com>
2020-10-27 14:13:14 +0800

    Gizmo

---
+++ src/main/java/com/kapcb/ccc/practice/think/Gizmo.java
@@ -0,0 +1,15 @@
+package com.kapcb.ccc.practice.think;
+
+/**
+ * @author kapcb
+ * @version 1.0.0
+ * @classname Gizmo
+ * @date 2020/10/27 13:26
+ */
+public class Gizmo {
+
+    public void spin() {
+        System.out.println("我被调用了");
+    }
+
+}
