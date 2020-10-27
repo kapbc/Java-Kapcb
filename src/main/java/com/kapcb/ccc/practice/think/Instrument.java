736fb3ff00baea93969ca67338b708d342ce56ad
kapcb <55942617+kapbc@users.noreply.github.com>
2020-10-27 11:40:46 +0800

    Instrument

---
+++ src/main/java/com/kapcb/ccc/practice/think/Instrument.java
@@ -0,0 +1,16 @@
+package com.kapcb.ccc.practice.think;
+
+/**
+ * @author kapcb
+ * @version 1.0.0
+ * @classname Instrument
+ * @date 2020/10/27 9:41
+ */
+public class Instrument {
+
+    public void play(){}
+
+    public static void tune(Instrument instrument){
+        instrument.play();
+    }
+}
