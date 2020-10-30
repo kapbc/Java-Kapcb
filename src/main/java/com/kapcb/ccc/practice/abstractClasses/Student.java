f4519b59e0c02bd154901ea6d14d4aa3f66913e4
kapcb <55942617+kapbc@users.noreply.github.com>
2020-10-30 11:03:07 +0800

    Student

---
+++ src/main/java/com/kapcb/ccc/practice/abstractClasses/Student.java
@@ -0,0 +1,21 @@
+package com.kapcb.ccc.practice.abstractClasses;
+
+/**
+ * @author kapcb
+ * @version 1.0.0
+ * @classname Student
+ * @date 2020/10/30 10:51
+ */
+public class Student extends Person {
+    private String major;
+
+    public Student(String name, String major) {
+        super(name);
+        this.major = major;
+    }
+
+    @Override
+    public String getDescription() {
+        return "A student majoring in " + major;
+    }
+}
