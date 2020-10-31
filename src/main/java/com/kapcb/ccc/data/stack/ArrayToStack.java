5ca56ce950185ac44c23900a0973804cdc9e1ab3
Eirccc <eircccallroot@yeah.net>
2020-10-31 09:38:53 +0800

    ArrayForStack

--- src/main/java/com/kapcb/ccc/data/stack/ArrayToStack.java
+++ src/main/java/com/kapcb/ccc/data/stack/ArrayToStack.java
@@ -15,17 +15,29 @@ public class ArrayToStack {
     private int index = -1;
     private static int[] stack;
 
+    /**
+     * 默认初始化容量
+     */
     public ArrayToStack() {
         this.capacity = 10;
         stack = new int[10];
     }
 
+    /**
+     * 自定义初始化容量
+     *
+     * @param capacity int
+     */
     public ArrayToStack(int capacity) {
         this.capacity = capacity;
         stack = new int[capacity];
     }
 
-
+    /**
+     * 压栈,index++
+     *
+     * @param element int
+     */
     public void push(int element) {
         if (isFull(stack)) {
             System.out.println("栈已满!压栈操作失败!");
@@ -35,6 +47,12 @@ public class ArrayToStack {
         stack[index] = element;
     }
 
+    /**
+     * 出栈,index--
+     *
+     * @return int
+     * @throws RuntimeException RuntimeException
+     */
     public int pop() throws RuntimeException {
         if (isEmpty(stack)) {
             System.out.println("栈空,无法弹栈!");
@@ -45,6 +63,9 @@ public class ArrayToStack {
         return temp;
     }
 
+    /**
+     * 获取栈中的数据
+     */
     public void getStackArray() {
         if (isEmpty(stack)) {
             System.out.println("栈空，无数据~");
@@ -55,12 +76,23 @@ public class ArrayToStack {
         }
     }
 
+    /**
+     * 判断栈满 true 满 / false 未满
+     *
+     * @param array int[]
+     * @return boolean
+     */
     private boolean isFull(int[] array) {
         return index == capacity - 1;
     }
 
+    /**
+     * 判断栈是否空 true 空 / false 否
+     *
+     * @param array int[]
+     * @return boolean
+     */
     private boolean isEmpty(int[] array) {
         return index == -1;
     }
-
 }
