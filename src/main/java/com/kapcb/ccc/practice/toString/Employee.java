23326773ac3814a8194bd99e74b192b522478ada
kapcb <55942617+kapbc@users.noreply.github.com>
2020-11-02 11:16:30 +0800

    Employee

---
+++ src/main/java/com/kapcb/ccc/practice/toString/Employee.java
@@ -0,0 +1,27 @@
+package com.kapcb.ccc.practice.toString;
+
+/**
+ * @author kapcb
+ * @version 1.0.0
+ * @classname Employee
+ * @date 2020/11/2 11:07
+ */
+public class Employee {
+
+    private String name;
+    private String password;
+    private String email;
+
+    public Employee(){}
+
+    public Employee(String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getName() + "[ name= " + name + ",password= " + password + ",email= " + email + "]";
+    }
+}
