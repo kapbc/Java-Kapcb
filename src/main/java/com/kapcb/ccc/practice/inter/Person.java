b541e90706e1ba5797187c76529ac4552c70087e
kapcb <55942617+kapbc@users.noreply.github.com>
2020-11-03 09:21:06 +0800

    Person

---
+++ src/main/java/com/kapcb/ccc/practice/inter/Person.java
@@ -0,0 +1,14 @@
+package com.kapcb.ccc.practice.inter;
+
+/**
+ * @author kapcb
+ * @version 1.0.0
+ * @classname Person
+ * @date 2020/11/3 9:14
+ */
+public interface Person {
+
+    default String getName() {
+        return "kapcb";
+    }
+}
