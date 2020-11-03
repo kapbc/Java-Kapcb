bda716c52ae8fcc7d229dad97f829f25882dd0da
kapcb <55942617+kapbc@users.noreply.github.com>
2020-11-03 09:35:22 +0800

    LengthComparator

---
+++ src/main/java/com/kapcb/ccc/practice/comparator/LengthComparator.java
@@ -0,0 +1,17 @@
+package com.kapcb.ccc.practice.comparator;
+
+import java.util.Comparator;
+
+/**
+ * @author kapcb
+ * @version 1.0.0
+ * @classname LengthComparator
+ * @date 2020/11/3 9:25
+ */
+public class LengthComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        return o1.length() - o2.length();
+    }
+}
