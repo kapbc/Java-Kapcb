d2663f3f7dee47cdee63192b86aa12f40b7a2b38
Eirccc <eircccallroot@yeah.net>
2020-11-09 20:18:45 +0800

    refactory

--- src/main/java/com/kapcb/ccc/leetcode/NoRepeatSubString.java
+++ src/main/java/com/kapcb/ccc/leetcode/NoRepeatSubString.java
@@ -8,13 +8,13 @@ import java.util.Map;
  * @version 1.0.0
  * @classname NoRepeatSubString
  * @date 2020/11/9 13:50
- *
+ * <p>
  * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度
  * 输入: "pwwkew"
  * 输出: 3
  * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
- *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
- *
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ * <p>
  * link: https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
  */
 public class NoRepeatSubString {
@@ -37,7 +37,7 @@ public class NoRepeatSubString {
                 start = Math.max(map.get(alpha), start);
             }
             count = Math.max(count, end - start + 1);
-            map.put(alpha, end+1);
+            map.put(alpha, end + 1);
         }
         return count;
     }
