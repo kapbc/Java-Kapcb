bbbf6dbf68b44bb64bd3c4da6d956893686540be
kapcb <55942617+kapbc@users.noreply.github.com>
2020-11-09 10:31:10 +0800

    ActionListener

---
+++ src/main/java/com/kapcb/ccc/inner/ActionListener.java
@@ -0,0 +1,18 @@
+package com.kapcb.ccc.practice.inner;
+
+import java.awt.event.ActionEvent;
+
+/**
+ * @author kapcb
+ * @version 1.0.0
+ * @classname ActionListener
+ * @date 2020/11/9 10:14
+ */
+public interface ActionListener {
+    /**
+     * 处理
+     *
+     * @param actionEvent ActionEvent
+     */
+    void actionPerformed(ActionEvent actionEvent);
+}
