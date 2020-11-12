5a15289825bba0c573e628a2ac999da03e2fe77f
kapcb <55942617+kapbc@users.noreply.github.com>
2020-11-12 16:57:55 +0800

    ContainerWithMostWater

---
+++ src/main/java/com/kapcb/ccc/leetcode/ContainerWithMostWater.java
@@ -0,0 +1,29 @@
+package com.kapcb.ccc.leetcode;
+
+
+
+/**
+ * <a>Title: ContainerWithMostWater </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/11/12-16:11
+ * <p>
+ * link: https://leetcode-cn.com/problems/container-with-most-water/
+ */
+public class ContainerWithMostWater {
+
+    public static void main(String[] args) {
+        int[] array = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int containerWithMostWater = getContainerWithMostWater(array);
+        System.out.println(containerWithMostWater);
+    }
+
+    private static int getContainerWithMostWater(int[] array) {
+        int maxX = 0;
+        int maxY = 0;
+
+    }
+}
