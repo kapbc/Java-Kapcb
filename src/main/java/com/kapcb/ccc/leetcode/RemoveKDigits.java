afdfe679dd7b1628543996440cf07de11ef8d532
Eirccc <eircccallroot@yeah.net>
2020-11-15 17:48:59 +0800

    RemoveKDigits

---
+++ src/main/java/com/kapcb/ccc/leetcode/RemoveKDigits.java
@@ -0,0 +1,27 @@
+package com.kapcb.ccc.leetcode;
+
+/**
+ * <a>Title: RemoveKDigits </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/11/15 - 17:46
+ * <p>
+ * 给定一个以字符串表示的非负整数 num，移除这个数中的 k 位数字，使得剩下的数字最小。
+ * 注意:
+ * num 的长度小于 10002 且 ≥ k。
+ * num 不会包含任何前导零。
+ * <p>
+ * 输入: num = "1432219", k = 3
+ * 输出: "1219"
+ * 解释: 移除掉三个数字 4, 3, 和 2 形成一个新的最小的数字 1219。
+ * <p>
+ * link: https://leetcode-cn.com/problems/remove-k-digits/
+ */
+public class RemoveKDigits {
+    public static void main(String[] args) {
+
+    }
+}
