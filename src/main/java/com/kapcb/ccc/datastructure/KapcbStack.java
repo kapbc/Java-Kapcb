436330d0ab3cbcaaecf6f818e7fbd829cccd6159
kapcb <55942617+kapbc@users.noreply.github.com>
2020-11-17 13:41:48 +0800

    KapcbStack By Kapcb

---
+++ src/main/java/com/kapcb/ccc/datastructure/KapcbStack.java
@@ -0,0 +1,83 @@
+package com.kapcb.ccc.datastructure;
+
+import java.util.logging.Logger;
+
+/**
+ * <a>Title: KapcbStack </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/11/17-13:14
+ */
+public class KapcbStack<E> {
+
+    private static final Logger logger = Logger.getLogger(String.valueOf(KapcbStack.class), "logmessage_en");
+
+    private int head = -1;
+    private int size = 0;
+    private int capacity;
+    private int[] array;
+
+    public KapcbStack() {
+        this.capacity = 10;
+        array = new int[capacity];
+    }
+
+    public KapcbStack(int capacity) {
+        this.capacity = capacity;
+        array = new int[capacity];
+    }
+
+    private boolean isFull(int[] array) {
+        return array.length == this.capacity ? true : false;
+    }
+
+    private boolean isEmpty(int[] array) {
+        return head == -1 ? true : false;
+    }
+
+    public <E extends Integer> void push(E e) {
+        if (isFull(array)) {
+            logger.warning("kapcb.stack.full.error");
+            return;
+        }
+        head++;
+        array[head] = e;
+        size++;
+    }
+
+    public int peek() {
+        if (isEmpty(array)) {
+            logger.warning("kapcb.stack.empty.error");
+            return 0;
+        }
+        return array[head];
+    }
+
+    public int pop() {
+        if (isEmpty(array)) {
+            logger.warning("kapcb.stack.empty.error");
+            return 0;
+        }
+        int temp = array[head];
+        head--;
+        size--;
+        return temp;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void list() {
+        if (isEmpty(array)) {
+            logger.warning("kapcb.stack.empty.error");
+            return;
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.printf("栈中的数据为:%d\n" + array[i]);
+        }
+    }
+}
