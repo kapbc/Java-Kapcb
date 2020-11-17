55b57a9518fee8646a877d8727d3089eec67bafc
kapcb <55942617+kapbc@users.noreply.github.com>
2020-11-17 17:29:00 +0800

    Create TreeSetTest.java

---
+++ src/main/java/com/kapcb/ccc/practice/collection/tree/TreeSetTest.java
@@ -0,0 +1,30 @@
+package com.kapcb.ccc.practice.collection.tree;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+/**
+ * <a>Title: TreeSetTest </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/11/17-17:23
+ */
+public class TreeSetTest {
+
+    public static void main(String[] args) {
+        TreeSet<Item> items = new TreeSet<>();
+        items.add(new Item(1997, "kapcb"));
+        items.add(new Item(1998, "aaaaa"));
+        items.add(new Item(1999, "ccccc"));
+        items.add(new Item(2000, "ddddd"));
+        items.add(new Item(2001, "eeeee"));
+        System.out.println(items);
+        System.out.println("============================");
+        TreeSet<Item> itemTreeSet = new TreeSet<>(Comparator.comparing(Item::getDescription));
+        itemTreeSet.addAll(items);
+        System.out.println(itemTreeSet);
+    }
+}
