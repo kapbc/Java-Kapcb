f28aca3fceeb20b2730666c90b7aa8013eb83b6c
kapcb <55942617+kapbc@users.noreply.github.com>
2020-11-17 14:22:42 +0800

    listIterator

--- src/main/java/com/kapcb/ccc/practice/collection/LinkedListTest.java
+++ src/main/java/com/kapcb/ccc/practice/collection/LinkedListTest.java
@@ -1,7 +1,10 @@
 package com.kapcb.ccc.practice.collection;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 
 /**
  * <a>Title: LinkedListTest </a>
@@ -25,6 +28,17 @@ public class LinkedListTest {
         iterator.remove();
         for (String s : linkedList) {
             System.out.println(s);
+        }
+
+        List<String> list = new ArrayList<>();
+        list.add("1111");
+        list.add("2222");
+        list.add("3333");
+        list.add("4444");
+        ListIterator<String> listIterator = list.listIterator();
+        while (listIterator.hasNext()) {
+            String previous = listIterator.next();
+            System.out.println(previous);
         }
     }
 }
