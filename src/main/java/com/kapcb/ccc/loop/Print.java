472e964eb892e0c546145f7c9f86d2c96693557d
kapcb <55942617+kapbc@users.noreply.github.com>
2020-11-18 09:41:35 +0800

    Print

---
+++ src/main/java/com/kapcb/ccc/loop/Print.java
@@ -0,0 +1,36 @@
+package com.kapcb.ccc.practice.loop;
+
+/**
+ * <a>Title: print </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/11/18-8:24
+ */
+public class Print {
+    private static final String SPACE = " ";
+    private static final String OPERATOR = "*";
+
+    public static void main(String[] args) {
+        getPrint(6);
+    }
+
+    private static void getPrint(int num) {
+        if (num < 0 || num == 0) {
+            return;
+        }
+        String result;
+        for (int i = 1; i <= num; i++) {
+            result = "" + OPERATOR;
+            for (int k = num - i; k >= 0; k--) {
+                result = result + SPACE;
+            }
+            for (int j = 1; j < i * 2; j++) {
+                result = result + OPERATOR;
+            }
+            System.out.println(result);
+        }
+    }
+}
