416a554000cddcda25fcefdbae3b4b83bc504423
kapcb <55942617+kapbc@users.noreply.github.com>
2020-11-19 15:32:56 +0800

    EnumElement

---
+++ src/main/java/com/kapcb/ccc/practice/collection/set/EnumElement.java
@@ -0,0 +1,20 @@
+package com.kapcb.ccc.practice.collection.set;
+
+/**
+ * <a>Title: EnumElement </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/11/19-15:26
+ */
+public enum EnumElement {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
