2249d0f3ab2a88194cc1e1642ce683824be419c3
kapcb <55942617+kapbc@users.noreply.github.com>
2020-11-20 13:45:55 +0800

    Create MaxInArray.java

---
+++ src/main/java/com/kapcb/ccc/practice/collection/MaxInArray.java
@@ -0,0 +1,35 @@
+package com.kapcb.ccc.practice.collection;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * <a>Title: MaxInArray </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/11/20-13:32
+ */
+public class MaxInArray {
+
+    private MaxInArray() {
+    }
+
+    public static <T extends Comparable> T getMaxInArray(Collection<T> collection) {
+        if (collection.isEmpty()) {
+            throw new NoSuchElementException("The Collection is empty! Please double check!");
+        }
+        Iterator<T> iterator = collection.iterator();
+        T largest = iterator.next();
+        while (iterator.hasNext()) {
+            T next = iterator.next();
+            if (next.compareTo(largest) > 0) {
+                largest = next;
+            }
+        }
+        return largest;
+    }
+}
