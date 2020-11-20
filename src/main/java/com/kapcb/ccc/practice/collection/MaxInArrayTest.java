17d5f478474c8422e6008b21987901743b67ed32
kapcb <55942617+kapbc@users.noreply.github.com>
2020-11-20 14:15:22 +0800

    Update MaxInArrayTest.java

--- src/main/java/com/kapcb/ccc/practice/collection/MaxInArrayTest.java
+++ src/main/java/com/kapcb/ccc/practice/collection/MaxInArrayTest.java
@@ -1,8 +1,10 @@
 package com.kapcb.ccc.practice.collection;
 
-import com.kapcb.ccc.practice.collection.MaxInArray;
+import com.kapcb.ccc.practice.think.Employee;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -25,5 +27,22 @@ public class MaxInArrayTest {
 
         Integer max = MaxInArray.getMaxInArray(list);
         System.out.println(max);
+
+        System.out.println("-----------------------");
+
+        List<Employee> employees = new LinkedList<>();
+        employees.add(new Employee("kapcb", 5000, 1997, 03, 11));
+        employees.add(new Employee("ccccc", 4000, 1998, 04, 13));
+        employees.add(new Employee("eeeee", 3000, 1999, 05, 15));
+        employees.add(new Employee("bbbbb", 2000, 1996, 06, 17));
+        employees.add(new Employee("aaaaa", 2500, 1995, 06, 23));
+        employees.add(new Employee("ddddd", 1000, 1994, 07, 12));
+        employees.sort(Comparator.comparingDouble(Employee::getSalary).reversed());
+        employees.forEach(System.out::println);
+
+        System.out.println("-----------------------");
+
+        employees.sort(Comparator.comparing(Employee::getHireDay));
+        employees.forEach(System.out::println);
     }
 }
