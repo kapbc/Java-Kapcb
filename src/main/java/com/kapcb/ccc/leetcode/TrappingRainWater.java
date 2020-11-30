65fcee654bcb2426858ce3ea647873f18120c55f
kapcb <55942617+kapbc@users.noreply.github.com>
2020-11-30 10:50:46 +0800

    TrappingRainWater

---
+++ src/main/java/com/kapcb/ccc/leetcode/TrappingRainWater.java
@@ -0,0 +1,24 @@
+package com.kapcb.ccc.leetcode;
+
+/**
+ * <a>Title: TrappingRainWater </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/11/30-10:45
+ * <p>
+ * link: https://leetcode-cn.com/problems/trapping-rain-water/
+ */
+public class TrappingRainWater {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static int trap(int[] array) {
+        // todo
+        return 0;
+    }
+}
