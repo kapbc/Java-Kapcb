2607255726e1cc99dbe4e61c7bd4d5db3a162910
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-03 14:12:37 +0800

    Update StackTraceTest.java

--- src/main/java/com/kapcb/ccc/practice/exception/StackTraceTest.java
+++ src/main/java/com/kapcb/ccc/practice/exception/StackTraceTest.java
@@ -1,38 +1,25 @@
 package com.kapcb.ccc.practice.exception;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 /**
  * @author kapcb
  * @version 1.0.0
- * @classname StackTraceTest
- * @date 2020/11/10 13:12
+ * @classname ExceptionTest
+ * @date 2020/11/10 9:53
  */
-public class StackTraceTest {
+public class ExceptionTest {
 
-    public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in);) {
-            System.out.println("Pls Entry an number:");
-            int i = scanner.nextInt();
-            factorial(i);
-        } finally {
-            System.out.println("Kapcb awesome man!!!");
-        }
+    private static final String EXCEPTION_CONDITION = "exception";
+
+    public static void main(String[] args) throws FileFormatException {
+        testException("exception");
     }
 
-    private static int factorial(int target) {
-        System.out.println("factorial(" + target + ")");
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        Arrays.stream(stackTrace).forEach(System.out::println);
-        int temp;
-        if (target <= 1) {
-            temp = 1;
+    private static void testException(String test) throws FileFormatException {
+        if (EXCEPTION_CONDITION.equals(test)) {
+            throw new FileFormatException("文件格式错误");
         } else {
-            temp = target * factorial(target - 1);
+            System.out.println("文件格式正确");
         }
-        System.out.println("return: " + temp);
-        return temp;
     }
 }
 
