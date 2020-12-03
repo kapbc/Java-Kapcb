9fd05fb75113382bca496ffacc83126f0c823f21
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-03 10:06:45 +0800

    util optimization

--- src/main/java/com/kapcb/ccc/util/KapcbStartUp.java
+++ src/main/java/com/kapcb/ccc/util/KapcbStartUp.java
@@ -1,6 +1,7 @@
 package com.kapcb.ccc.util;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.Properties;
 import java.util.logging.Logger;
 
@@ -25,6 +26,9 @@ public class KapcbStartUp {
     private KapcbStartUp() {
     }
 
+    /**
+     * 调用kapcb nb 启动logo
+     */
     public static void kapcbUp() {
         Properties properties = new Properties();
         try (FileInputStream fileInputStream = new FileInputStream("src/main/resource/start.properties");) {
@@ -35,6 +39,51 @@ public class KapcbStartUp {
             }
         } catch (Exception e) {
             logger.warning("kapcb.start.up.fail");
+            logger.warning("System Exception: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Please check whether the StartUp have been insert into the properties
+     * 
+     * kapcb.start.up.0=---------------------------Start Up Begin------------------------------------
+     * kapcb.start.up.1=|__    __                                __              __     __  __      
+     * kapcb.start.up.2=/  |  /  |                              /  |            /      /  |/  |      
+     * kapcb.start.up.3=$$ | /$$/   ______    ______    _______ $$ |____        $$     $$ |$$ |_____ 
+     * kapcb.start.up.4=$$ |/$$/   /        /          /       |$$       |      $$$    $$ |$$       |
+     * kapcb.start.up.5=$$  $$<     $$$$$$ |/$$$$$$  |/$$$$$$$/ $$$$$$$  |      $$$$   $$ |$$$$$$$  |
+     * kapcb.start.up.6=$$$$$      /    $$ |$$ |  $$ |$$ |   $$|$$ |  $$ |      $$ $$  $$ |$$|   $$ |
+     * kapcb.start.up.7=$$ |$$    /$$$$$$$ |$$ |__$$ |$$     __|$$ |__$$ |      $$ |$$ $$ |$$|___$$ |
+     * kapcb.start.up.8=$$ | $$  | $$   $$ |$$    $$/ $$       |$$    $$/       $$ | $$$$ |$$    $$/
+     * kapcb.start.up.9=$$/   $$/  $$$$$$$/ $$$$$$$/   $$$$$$$/ $$$$$$$/        $$/   $$/  $$$$$$$/ 
+     * kapcb.start.up.10=|                   $$ |                                                    
+     * kapcb.start.up.11=|                   $$ |                                                    
+     * kapcb.start.up.12=|                   $$ /  
+     * kapcb.start.up.13=---------------------------Start Up Success----------------------------------
+     * 
+     * 
+     */
+//    public static void kapcbStartUpPropertiesInsert() {
+    private static void kapcbStartUpPropertiesInsert() {
+        Properties properties = new Properties();
+        properties.setProperty("kapcb.start.up.0", "---------------------------Start Up Begin------------------------------------");
+        properties.setProperty("kapcb.start.up.1", "|__    __                                __              __     __  __      ");
+        properties.setProperty("kapcb.start.up.2", "/  |  /  |                              /  |            /      /  |/  |      ");
+        properties.setProperty("kapcb.start.up.3", "$$ | /$$/   ______    ______    _______ $$ |____        $$     $$ |$$ |_____ ");
+        properties.setProperty("kapcb.start.up.4", "$$ |/$$/   /        /          /       |$$       |      $$$    $$ |$$       |");
+        properties.setProperty("kapcb.start.up.5", "$$  $$<     $$$$$$ |/$$$$$$  |/$$$$$$$/ $$$$$$$  |      $$$$   $$ |$$$$$$$  |");
+        properties.setProperty("kapcb.start.up.6", "$$$$$      /    $$ |$$ |  $$ |$$ |   $$|$$ |  $$ |      $$ $$  $$ |$$|   $$ |");
+        properties.setProperty("kapcb.start.up.7", "$$ |$$    /$$$$$$$ |$$ |__$$ |$$     __|$$ |__$$ |      $$ |$$ $$ |$$|___$$ |");
+        properties.setProperty("kapcb.start.up.8", "$$ | $$  | $$   $$ |$$    $$/ $$       |$$    $$/       $$ | $$$$ |$$    $$/");
+        properties.setProperty("kapcb.start.up.9", "$$/   $$/  $$$$$$$/ $$$$$$$/   $$$$$$$/ $$$$$$$/        $$/   $$/  $$$$$$$/");
+        properties.setProperty("kapcb.start.up.10", "|                   $$ |                                                    ");
+        properties.setProperty("kapcb.start.up.11", "|                   $$ |                                                    ");
+        properties.setProperty("kapcb.start.up.12", "|                   $$ |                                                    ");
+        properties.setProperty("kapcb.start.up.13", "---------------------------Start Up Success----------------------------------");
+        try (FileOutputStream fileOutputStream = new FileOutputStream("src/main/resource/start.properties");) {
+            properties.store(fileOutputStream, "#########################################kapcb start up########################################");
+        } catch (Exception e) {
+            logger.warning("kapcb.start.up.store.fail");
             logger.warning("System Exception: " + e.getMessage());
         }
     }
