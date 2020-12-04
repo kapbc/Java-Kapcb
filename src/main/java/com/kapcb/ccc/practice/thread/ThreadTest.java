b8ef9f86028bcb321af5c89f5921d6d2dc566f74
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-04 13:19:07 +0800

    Update ThreadTest.java

--- src/main/java/com/kapcb/ccc/practice/thread/ThreadTest.java
+++ src/main/java/com/kapcb/ccc/practice/thread/ThreadTest.java
@@ -1,53 +1,54 @@
-package com.kapcb.ccc.practice.thread;
+package com.kapcb.ccc.practice.thread.synchroniz;
 
 import com.kapcb.ccc.util.KapcbStartUp;
 
 import java.util.logging.Logger;
 
 /**
- * <a>Title: ThreadTest </a>
+ * <a>Title: BankTest </a>
  * <a>Author: kapcb <a>
  * <a>Description：<a>
  *
  * @author kapcb
  * @version 1.0.0
- * @date 2020/12/1-17:03
+ * @date 2020/12/4-10:56
  */
-public class ThreadTest {
+public class BankTest {
+
+    private static final Logger logger = Logger.getLogger(String.valueOf(BankTest.class), "logmessage_en");
 
     private static final int DELAY = 10;
     private static final int STEPS = 100;
     private static final double MAX_ACCOUNT = 1000;
 
-    private static final Logger logger = Logger.getLogger(String.valueOf(ThreadTest.class), "logmessage_en");
-
     public static void main(String[] args) {
         KapcbStartUp.kapcbUp();
+
         Bank bank = new Bank(4, 10000);
 
         new Thread(() -> {
-            try {
-                for (int i = 0; i < STEPS; i++) {
-                    double amount = MAX_ACCOUNT * Math.random();
+            for (int i = 0; i < STEPS; i++) {
+                double amount = MAX_ACCOUNT * Math.random();
+                try {
                     bank.transfer(0, 1, amount);
-                    int sleepTime = (int) (DELAY * Math.random());
-                    Thread.sleep(sleepTime);
+                    Thread.sleep((int) (DELAY * Math.random()));
+                } catch (InterruptedException e) {
+                    logger.warning("kapcb.thread.test.exception");
+                    logger.warning("Exception Information: " + e.getMessage());
                 }
-            } catch (InterruptedException e) {
-                logger.warning("System exception= " + e.getMessage());
             }
         }, "Thread A").start();
 
         new Thread(() -> {
-            try {
-                for (int i = 0; i < STEPS; i++) {
-                    double amount = MAX_ACCOUNT * Math.random();
+            for (int i = 0; i < STEPS; i++) {
+                double amount = MAX_ACCOUNT * Math.random();
+                try {
                     bank.transfer(2, 3, amount);
-                    int sleepTime = (int) (DELAY * Math.random());
-                    Thread.sleep(sleepTime);
+                    Thread.sleep((int) (DELAY * Math.random()));
+                } catch (InterruptedException e) {
+                    logger.warning("kapcb.thread.test.exception");
+                    logger.warning("Exception Information: " + e.getMessage());
                 }
-            } catch (InterruptedException e) {
-                logger.warning("System exception= " + e.getMessage());
             }
         }, "Thread B").start();
     }
