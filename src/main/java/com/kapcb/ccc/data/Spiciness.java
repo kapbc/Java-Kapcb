5cb04d96f66a146f7948d4d24bafcd208a5eb4fa
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-07 09:47:54 +0800

    Update Spiciness.java

--- src/main/java/com/kapcb/ccc/data/Spiciness.java
+++ src/main/java/com/kapcb/ccc/data/Spiciness.java
@@ -4,7 +4,7 @@ package com.kapcb.ccc.data;
  * @author kapcb
  * @version 1.0.0
  * @classname Spiciness
- * @description TODO
+ * @description 
  * @date 2020/10/22 15:58
  */
 public enum Spiciness {
