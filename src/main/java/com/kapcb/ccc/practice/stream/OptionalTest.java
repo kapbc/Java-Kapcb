bfc5e70aa5fdaa5fa2a8265ce05013acf3083fd7
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-09 13:31:18 +0800

    Create OptionalTest.java

---
+++ src/main/java/com/kapcb/ccc/practice/stream/OptionalTest.java
@@ -0,0 +1,17 @@
+package com.kapcb.ccc.practice.stream;
+
+/**
+ * <a>Title: OptionalTest </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/12/9-13:29
+ */
+public class OptionalTest {
+
+    public static void main(String[] args) {
+
+    }
+}
