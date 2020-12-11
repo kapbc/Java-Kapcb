f062318ab40279b7366d494c81097616124ae451
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-11 11:47:06 +0800

    Update Employee.java

--- src/main/java/com/kapcb/ccc/practice/think/Employee.java
+++ src/main/java/com/kapcb/ccc/practice/think/Employee.java
@@ -14,7 +14,10 @@ public class Employee {
     private double salary;
     private LocalDate hireDay;
 
-    public Employee() {
+    /**
+     * I Hope My Project Won't Tips Too Many Objects That I Don't Want To Use When I Am Coding! Just For Test Data!
+     */
+    private Employee() {
     }
 
     public Employee(String name) {
