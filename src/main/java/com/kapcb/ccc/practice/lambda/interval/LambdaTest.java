fe07d5bf794dcd5318279940e307b2fe900a0026
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-11 08:42:26 +0800

    LambdaTest

---
+++ src/main/java/com/kapcb/ccc/practice/lambda/interval/LambdaTest.java
@@ -0,0 +1,19 @@
+package com.kapcb.ccc.practice.lambda.interval;
+
+/**
+ * <a>Title: LambdaTest </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/12/1-10:22
+ */
+public class LambdaTest {
+
+    public static void main(String[] args) {
+        RepeatedGreeter repeatedGreeter = new RepeatedGreeter();
+        repeatedGreeter.greet();
+    }
+}
+
