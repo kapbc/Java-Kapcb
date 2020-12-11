6a5d402e4a173d0e076e628039a5b705b48c88c0
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-11 13:09:59 +0800

    Update Employee.java

--- src/main/java/com/kapcb/ccc/practice/toString/Employee.java
+++ src/main/java/com/kapcb/ccc/practice/toString/Employee.java
@@ -12,10 +12,7 @@ public class Employee {
     private String password;
     private String email;
 
-    /**
-     * I Hope My Project Won't Tips Too Many Objects That I Don't Want To Use When I Am Coding! Just For Test Data!
-     */
-    private Employee() {
+    public Employee() {
     }
 
     public Employee(String name, String password, String email) {
