2d4e274c94f896bfcf47f64c32420639c7a4e56d
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-11 13:08:58 +0800

    Update Person.java

--- src/main/java/com/kapcb/ccc/practice/abstractClasses/Person.java
+++ src/main/java/com/kapcb/ccc/practice/abstractClasses/Person.java
@@ -12,10 +12,7 @@ public abstract class Person {
 
     public abstract String getDescription();
 
-    /**
-     * I Hope My Project Won't Tips Too Many Objects That I Don't Want To Use When I Am Coding! Just For Test Data!
-     */
-    private Person() {
+    public Person() {
     }
 
     public Person(String username) {
