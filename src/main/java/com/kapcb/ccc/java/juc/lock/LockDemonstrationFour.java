b315d6f908961e1bd0accb874aa2a2dfab6f7e55
kapbc <2530628867@qq.com>
2020-12-12 23:13:12 +0800

    Optimization

---
+++ src/main/java/com/kapcb/ccc/java/juc/lock/LockDemonstrationFour.java
@@ -0,0 +1,22 @@
+package com.kapcb.ccc.java.juc.lock;
+
+/**
+ * <a>Title: LockDemonstrationFour </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/12/12 - 23:12
+ */
+public class LockDemonstrationFour {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    private static class Phone {
+
+    }
+}
