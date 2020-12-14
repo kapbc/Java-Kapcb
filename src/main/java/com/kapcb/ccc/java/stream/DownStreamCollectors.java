c7a99b57cd32eec1e6e4f99bde8143e6e75d1efd
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-14 13:50:54 +0800

    DownStreamCollectors

---
+++ src/main/java/com/kapcb/ccc/java/stream/DownStreamCollectors.java
@@ -0,0 +1,17 @@
+package com.kapcb.ccc.java.stream;
+
+/**
+ * <a>Title: DownStreamCollectors </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/12/14-13:50
+ */
+public class DownStreamCollectors {
+
+    public static void main(String[] args) {
+        
+    }
+}
