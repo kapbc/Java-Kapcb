feb73b20ee70c92f26ee23cf4bf9b8593271f579
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-14 16:40:43 +0800

    Create PrimitiveTypeStreams.java

---
+++ src/main/java/com/kapcb/ccc/java/stream/PrimitiveTypeStreams.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.java.stream;
+
+/**
+ * <a>Title: PrimitiveTypeStreams </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/12/14-16:40
+ */
+public class PrimitiveTypeStreams {
+}
