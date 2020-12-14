d5bd6b00931d998b408b667525073db4d85366b5
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-14 13:49:28 +0800

    Update DownStreamCollector.java

--- src/main/java/com/kapcb/ccc/java/stream/DownStreamCollector.java
+++ src/main/java/com/kapcb/ccc/java/stream/DownStreamCollector.java
@@ -13,12 +13,12 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
 import static java.util.stream.Collectors.maxBy;
 import static java.util.stream.Collectors.summarizingInt;
 import static java.util.stream.Collectors.toSet;
@@ -36,6 +36,7 @@ public class DownStreamCollector {
 
     public static void main(String[] args) throws IOException {
         Path path = Paths.get(Constants.COMMON_CITY_TXT_RESOURCES_PATH.getStringStatusCode());
+        Path pathForStreamTxt = Paths.get(Constants.COMMON_STREAM_TEST_TXT_RESOURCES_PATH.getStringStatusCode());
         Stream<Locale> availableLocales = Stream.of(Locale.getAvailableLocales());
         Map<String, Set<Locale>> collect = availableLocales.collect(groupingBy(Locale::getCountry, toSet()));
 
@@ -56,9 +57,31 @@ public class DownStreamCollector {
         Optional<City> tx = stateToLargestCity.get("TX");
         System.out.println(tx.orElse(null));
 
-        // Stream<City> cityStreamTwo = Files.lines(path).map(element -> element.split(Constants.COMMON_STRING_SPLIT_COMMA.getStringStatusCode())).map(line -> new City(line[0], line[1], Integer.parseInt(line[2])));
-        Stream<String> lines = Files.lines(Paths.get(Constants.COMMON_STREAM_TEST_TXT_RESOURCES_PATH.getStringStatusCode()), StandardCharsets.UTF_8);
+        Stream<String> lines = Files.lines(pathForStreamTxt, StandardCharsets.UTF_8);
         Map<Character, Integer> characterIntegerMap = lines.collect(groupingBy(s -> s.charAt(0), collectingAndThen(toSet(), Set::size)));
         characterIntegerMap.forEach((k, v) -> System.out.println("k: " + k + ", v: " + v));
+
+
+        Stream<String> linesStream = Files.lines(pathForStreamTxt, StandardCharsets.UTF_8);
+        Map<Character, Set<Integer>> characterSetMap = linesStream.collect(groupingBy(s -> s.charAt(0), mapping(String::length, toSet())));
+        characterSetMap.forEach((k, v) -> System.out.println("k: " + k + ", v: " + v));
+
+        Stream<Locale> stream = Stream.of(Locale.getAvailableLocales());
+        Map<String, Set<String>> setMap = stream.collect(groupingBy(Locale::getDisplayCountry, mapping(Locale::getDisplayLanguage, toSet())));
+        // setMap.forEach((k, v) -> System.out.println("k: " + k + ", v: " + v));
+        Set<String> china = setMap.get("中国");
+        System.out.println(china);
+
+        Stream<City> cityStreamTwo = Files.lines(path, StandardCharsets.UTF_8).map(s -> s.split(Constants.COMMON_STRING_SPLIT_COMMA.getStringStatusCode())).map(city -> new City(city[0], city[1], Integer.parseInt(city[2])));
+        Map<String, IntSummaryStatistics> stringIntSummaryStatisticsMap = cityStreamTwo.collect(groupingBy(City::getState, summarizingInt(City::getPopulation)));
+        stringIntSummaryStatisticsMap.forEach((k, v) -> System.out.println("k: " + k + ", v: " + v));
+
+        /**
+         * Since Java 9
+         */
+        // Stream<City> cityStreamThree = Files.lines(path, StandardCharsets.UTF_8).map(s -> s.split(Constants.COMMON_STRING_SPLIT_COMMA.getStringStatusCode())).map(a -> new City(a[0], a[1], Integer.parseInt(a[2])));
+        // cityStreamThree.collect(groupingBy(City::getState, filtering(c -> c.getPopulation) > 5000, toSet()));
+        
+
     }
 }
