1d502051b10ec6818b8afe475ef3cb237de9c394
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-15 16:37:55 +0800

    MonotoneIncreaseDigits

---
+++ src/main/java/com/kapcb/ccc/algorithm/leetcode/secondary/MonotoneIncreaseDigits.java
@@ -0,0 +1,19 @@
+package com.kapcb.ccc.algorithm.leetcode.secondary;
+
+/**
+ * <a>Title: MonotoneIncreaseDigits </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/12/15-16:36
+ *
+ * link: https://leetcode-cn.com/problems/monotone-increasing-digits/
+ */
+public class MonotoneIncreaseDigits {
+
+    public static void main(String[] args) {
+
+    }
+}
