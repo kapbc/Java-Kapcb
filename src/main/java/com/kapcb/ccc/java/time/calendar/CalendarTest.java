449595cf0c1f252a9f695669c585c9ea33063d02
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-18 08:56:45 +0800

    Update CalendarTest.java

--- src/main/java/com/kapcb/ccc/java/time/calendar/CalendarTest.java
+++ src/main/java/com/kapcb/ccc/java/time/calendar/CalendarTest.java
@@ -21,7 +21,21 @@ public class CalendarTest {
         Calendar calendar = Calendar.getInstance(Locale.CHINA);
         // 获取当前时间并格式化
         String currentTime = simpleDateFormat.format(calendar.getTime());
-        System.out.println("currentTime = " + currentTime);
-    }
+        System.out.println("Current Time is: " + currentTime);
 
+        // 给当前时间加上天数days并输出, 第一个参数用以下三个都可以
+        int addDays = 2;
+        calendar.add(Calendar.DAY_OF_YEAR, addDays);
+        currentTime = simpleDateFormat.format(calendar.getTime());
+        System.out.println("The Time That Have Added " + addDays + " Days Is : " + currentTime);
+
+        calendar.add(Calendar.DAY_OF_MONTH, addDays);
+        currentTime = simpleDateFormat.format(calendar.getTime());
+        System.out.println("The Time That Have Added " + addDays + " Days Is : " + currentTime);
+
+        calendar.add(Calendar.DAY_OF_WEEK,addDays);
+        currentTime = simpleDateFormat.format(calendar.getTime());
+        System.out.println("The Time That Have Added " + addDays + " Days Is : " + currentTime);
+        
+    }
 }
