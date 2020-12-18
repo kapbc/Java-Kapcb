bc85ae7b03ac7a8a4059a0337226d708676a53c6
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-18 08:47:56 +0800

    Update CalendarTest.java

--- src/main/java/com/kapcb/ccc/java/time/calendar/CalendarTest.java
+++ src/main/java/com/kapcb/ccc/java/time/calendar/CalendarTest.java
@@ -1,5 +1,9 @@
 package com.kapcb.ccc.java.time.calendar;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 /**
  * <a>Title: CalendarTest </a>
  * <a>Author: kapcb <a>
@@ -11,5 +15,13 @@ package com.kapcb.ccc.java.time.calendar;
  */
 public class CalendarTest {
 
+
+    public static void main(String[] args) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar calendar = Calendar.getInstance(Locale.CHINA);
+        // 获取当前时间并格式化
+        String currentTime = simpleDateFormat.format(calendar.getTime());
+        System.out.println("currentTime = " + currentTime);
+    }
 
 }
