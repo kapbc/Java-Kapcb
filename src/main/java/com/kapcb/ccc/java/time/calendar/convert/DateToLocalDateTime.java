c113337351bfc90cc4ebc2e64d7460ffb74c0b36
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-18 10:24:03 +0800

    DateToLocalDateTime

---
+++ src/main/java/com/kapcb/ccc/java/time/calendar/convert/DateToLocalDateTime.java
@@ -0,0 +1,25 @@
+package com.kapcb.ccc.java.time.convert;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+/**
+ * <a>Title: DateToLocalDateTime </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/12/18-10:20
+ */
+public class DateToLocalDateTime {
+
+    public static void main(String[] args) {
+        Date todayDate = new Date();
+        System.out.println("todayDate = " + todayDate);
+
+        LocalDateTime localDateTime = todayDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        System.out.println("localDateTime = " + localDateTime);
+    }
+}
