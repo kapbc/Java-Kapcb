2328a6c8b93a0278fdf4e868a189735ccd7dcbea
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-21 09:37:02 +0800

    Update After.java

--- src/main/java/com/kapcb/ccc/java/time/calendar/After.java
+++ src/main/java/com/kapcb/ccc/java/time/calendar/After.java
@@ -3,8 +3,10 @@ package com.kapcb.ccc.java.time.calendar;
 import com.kapcb.ccc.util.Constants;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * <a>Title: After </a>
@@ -30,6 +32,21 @@ public class After {
         boolean after = now.isAfter(parse);
         System.out.println("after = " + after);
 
-        Calendar instance = Calendar.getInstance();
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, -7);
+        calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        Date time = calendar.getTime();
+        System.out.println("time = " + time);
+        LocalDateTime afterPervWeekMonday = LocalDateTime.of(2020, 12, 14, 0, 0, 1, 120);
+        LocalDateTime beforePervWeekMonday = LocalDateTime.of(2020, 12, 13, 23, 59, 59, 999);
+        Date afterPervWeekMondayDate = Date.from(afterPervWeekMonday.atZone(ZoneId.systemDefault()).toInstant());
+        System.out.println("afterPervWeekMondayDate = " + afterPervWeekMondayDate);
+        Date beforePervWeekMondayDate = Date.from(beforePervWeekMonday.atZone(ZoneId.systemDefault()).toInstant());
+        System.out.println("beforePervWeekMondayDate = " + beforePervWeekMondayDate);
+        System.out.println("After : " + afterPervWeekMondayDate.after(time));
+        System.out.println("Before : " + beforePervWeekMondayDate.after(time));
     }
 }
