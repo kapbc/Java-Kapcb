5013a6c6e440c7830d6cd03f8079cb5ad0ba39f1
kapbc <2530628867@qq.com>
2020-12-22 21:55:55 +0800

    GetName

---
+++ src/main/java/com/kapcb/ccc/java/reflect/GetClassName.java
@@ -0,0 +1,20 @@
+package com.kapcb.ccc.java.reflect;
+
+/**
+ * <a>Title: GetClassName </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/12/22 - 21:55
+ */
+public class GetClassName {
+
+    public static void main(String[] args) {
+
+        Class<Person> personClass = Person.class;
+        String name = personClass.getName();
+        System.out.println("name = " + name);
+    }
+}
