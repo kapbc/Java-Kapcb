38dd8f669cbf46fa81aa50d867f1de3fb3f3be78
kapcb <55942617+kapbc@users.noreply.github.com>
2020-12-28 16:52:18 +0800

    Create Person.java

---
+++ src/main/java/com/kapcb/ccc/java/reflect/tdd/Person.java
@@ -0,0 +1,52 @@
+package com.kapcb.ccc.java.reflect.tdd;
+
+
+/**
+ * <a>Title: Person </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2020/12/28-16:07
+ */
+public class Person {
+
+    @Column(name = "user_name")
+    private String username;
+
+    @Column(name = "pass_word")
+    private String password;
+
+    public Person() {
+    }
+
+    public Person(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
