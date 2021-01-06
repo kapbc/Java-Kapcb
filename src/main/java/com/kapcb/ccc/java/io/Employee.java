25c80240afc008271e07d69afd06b2be6f1cfd7d
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-06 16:53:24 +0800

    Update Employee.java

--- src/main/java/com/kapcb/ccc/java/io/Employee.java
+++ src/main/java/com/kapcb/ccc/java/io/Employee.java
@@ -1,5 +1,7 @@
 package com.kapcb.ccc.java.io;
 
+import java.time.LocalDate;
+
 /**
  * <a>Title: Manager </a>
  * <a>Author: kapcb <a>
@@ -9,32 +11,56 @@ package com.kapcb.ccc.java.io;
  * @version 1.0.0
  * @date 2021/1/6-14:20
  */
-public class Manager extends Employee {
+public class Employee {
 
-    private Employee secretary;
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
 
-    public Manager() {
-        super();
-        secretary = null;
+    public Employee() {
     }
 
-    public Manager(String name, double salary, int year, int month, int day) {
-        super(name, salary, year, month, day);
-        secretary = null;
+    public Employee(String name, double salary, int year, int month, int day) {
+        this.name = name;
+        this.salary = salary;
+        this.hireDay = LocalDate.of(year, month, day);
     }
 
-    public void setSecretary(Employee secretary) {
-        this.secretary = secretary;
+    public String getName() {
+        return name;
     }
 
-    public Employee getSecretary() {
-        return this.secretary;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getHireDay() {
+        return hireDay;
+    }
+
+    public void setHireDay(LocalDate hireDay) {
+        this.hireDay = hireDay;
+    }
+
+    public void raiseSalary(int byPresent) {
+        double raise = this.salary * byPresent / 100;
+        salary += raise;
     }
 
     @Override
     public String toString() {
-        return  super.toString() +
-                " secretary=" + secretary +
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                ", hireDay=" + hireDay +
                 '}';
     }
 }
