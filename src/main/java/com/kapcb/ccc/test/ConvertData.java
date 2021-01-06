a24804e8ae152e2b9a256c9229371202ec788500
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-06 13:54:25 +0800

    Update ConvertData.java

--- src/main/java/com/kapcb/ccc/test/ConvertData.java
+++ src/main/java/com/kapcb/ccc/test/ConvertData.java
@@ -2,6 +2,8 @@ package com.kapcb.ccc.test;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
 
@@ -17,6 +19,7 @@ import java.util.function.Function;
 public class ConvertData {
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss");
 
     public static void main(String[] args) {
         LocalDateTime now = LocalDateTime.now();
@@ -39,12 +42,28 @@ public class ConvertData {
         System.out.println("format = " + format);
         String s4 = Objects.requireNonNull(A);
         System.out.println("s4 = " + s4);
-        String C =null;
-        String s5 = Objects.requireNonNull(C);
-        System.out.println("s5 = " + s5);
+        String C = null;
+        //String s5 = Objects.requireNonNull(C);
+        //System.out.println("s5 = " + s5);
+
+        LocalDateTime now1 = LocalDateTime.now();
+        System.out.println("now1 = " + now1);
+        String format1 = now1.format(FORMATTER);
+        System.out.println("format1 = " + format1);
+
+        List<String> kapcb = testGet("kapcb");
+        System.out.println(kapcb == null);
+        System.out.println("kapcb.size() = " + kapcb.size());
     }
 
     private static <T> String convertData(T data, Function<T, String> function) {
         return data == null ? null : function.apply(data);
+    }
+
+    private static List<String> testGet(String name) {
+        if (name != null) {
+            System.out.println("name = " + name);
+        }
+        return new ArrayList<String>();
     }
 }
