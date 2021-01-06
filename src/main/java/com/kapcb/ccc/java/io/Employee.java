35c04142d4ddf850ccb7246aae23ae14af819127
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-06 14:09:33 +0800

    Create Employee.java

---
+++ src/main/java/com/kapcb/ccc/java/io/Employee.java
@@ -0,0 +1,66 @@
+package com.kapcb.ccc.java.io;
+
+import java.time.LocalDate;
+
+/**
+ * <a>Title: Employee </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/1/6-14:06
+ */
+public class Employee extends SerialCloneable {
+
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+
+    public Employee() {
+    }
+
+    public Employee(String name, double salary, LocalDate hireDay) {
+        this.name = name;
+        this.salary = salary;
+        this.hireDay = hireDay;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getHireDay() {
+        return hireDay;
+    }
+
+    public void setHireDay(LocalDate hireDay) {
+        this.hireDay = hireDay;
+    }
+
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                ", hireDay=" + hireDay +
+                '}';
+    }
+}
