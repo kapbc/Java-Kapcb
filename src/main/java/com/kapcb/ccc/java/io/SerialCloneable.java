900424382482b4af65a2ea1846d4818b79c0ff46
kapcb <55942617+kapbc@users.noreply.github.com>
2021-01-06 14:06:39 +0800

    Create SerialCloneable.java

---
+++ src/main/java/com/kapcb/ccc/java/io/SerialCloneable.java
@@ -0,0 +1,40 @@
+package com.kapcb.ccc.java.io;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/**
+ * <a>Title: SerialCloneable </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/1/6-14:00
+ */
+public class SerialCloneable implements Cloneable, Serializable {
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream)) {
+
+                objectOutputStream.writeObject(this);
+
+            }
+            try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray())) {
+                ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+                return objectInputStream.readObject();
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            CloneNotSupportedException cloneNotSupportedException = new CloneNotSupportedException();
+            cloneNotSupportedException.initCause(e);
+            throw cloneNotSupportedException;
+        }
+    }
+}
