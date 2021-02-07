2556aa6f218a299d421f2849de92a5676d0c3f4d
kapcb <55942617+kapbc@users.noreply.github.com>
2021-02-07 14:53:00 +0800

    Create StrategyTest.java

---
+++ src/main/java/com/kapcb/ccc/pattern/strategy/StrategyTest.java
@@ -0,0 +1,21 @@
+package com.kapcb.ccc.pattern.strategy;
+
+/**
+ * <a>Title: StrategyTest </a>
+ * <a>Author: kapcb <a>
+ * <a>Description：<a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/7-14:48
+ */
+public class StrategyTest {
+
+    public static void main(String[] args) {
+        PromotionActivity couponActivity = new PromotionActivity(new CouponStrategy());
+        PromotionActivity cashbackActivity = new PromotionActivity(new CashbackStrategy());
+
+        couponActivity.execute();
+        cashbackActivity.execute();
+    }
+}
