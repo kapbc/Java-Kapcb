d5f6d3699b38e4fae358ff39a493441917b41a6c
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-01-16 22:43:09 +0800

    add jvm

---
+++ src/main/java/com/kapcb/ccc/jvm/classload/HelloClassLoader.java
@@ -0,0 +1,14 @@
+package com.kapcb.ccc.jvm.classload;
+
+/**
+ * <a>Title: HelloClassLoader </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: HelloClassLoader <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/1/16 22:27
+ * @since 1.0
+ */
+public class HelloClassLoader {
+}
