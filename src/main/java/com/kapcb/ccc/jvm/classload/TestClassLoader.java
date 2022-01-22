a4e227fee18a9226a814bdc0a61872b9da963948
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-01-22 23:49:29 +0800

    TestClassLoader

--- src/main/java/com/kapcb/ccc/jvm/classload/TestClassLoader.java
+++ src/main/java/com/kapcb/ccc/jvm/classload/TestClassLoader.java
@@ -1,6 +1,7 @@
 package com.kapcb.ccc.jvm.classload;
 
 import sun.misc.Launcher;
+import sun.security.ec.ECKeyFactory;
 
 import java.net.Proxy;
 import java.net.URL;
@@ -24,16 +25,29 @@ public class TestClassLoader {
     public static void main(String[] args) {
         URL[] urLs = Launcher.getBootstrapClassPath().getURLs();
         Arrays.stream(urLs).forEach(System.out::println);
+        System.out.println("\n");
 
         // 从上面的路劲中随便挑选一个类, 查看其类加载器 :
         ClassLoader proxyClassClassLoader = Proxy.class.getClassLoader();
         // null --> BootStrap Class Loader
         System.out.println("proxyClassClassLoader = " + proxyClassClassLoader);
 
+        System.out.println("\n");
         System.out.println("===========extension class loader spilt line===========");
+
+        // 寻找 jre/lib/ext 目录下的 class 获取 ClassLoader
+        ClassLoader ecKeyFactoryClassLoader = ECKeyFactory.class.getClassLoader();
+        // sun.misc.Launcher$ExtClassLoader@7cca494b
+        System.out.println("ecKeyFactoryClassLoader = " + ecKeyFactoryClassLoader);
+
+        System.out.println("\n");
         String extensionUrl = System.getProperty(EXT_URL);
         Arrays.stream(extensionUrl.split(SEMICOLON)).forEach(System.out::println);
 
+        // 获取自定义类的 ClassLoader
+        ClassLoader classLoader = CustomClassLoader.class.getClassLoader();
+        // sun.misc.Launcher$AppClassLoader@18b4aac2
+        System.out.println("classLoader = " + classLoader);
     }
 
 }
