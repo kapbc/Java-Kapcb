4839135868c48da209eee904481a48dbf4fc6814
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-01-22 23:36:23 +0800

    add TestClassLoader

---
+++ src/main/java/com/kapcb/ccc/jvm/classload/TestClassLoader.java
@@ -0,0 +1,34 @@
+package com.kapcb.ccc.jvm.classload;
+
+import sun.misc.Launcher;
+
+import java.net.URL;
+import java.util.Arrays;
+
+/**
+ * <a>Title: TestClassLoader </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: TestClassLoader <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/1/22 23:28
+ * @since 1.0
+ */
+public class TestClassLoader {
+
+    private static final String EXT_URL = "java.ext.dirs";
+    private static final String SEMICOLON = ";";
+
+    public static void main(String[] args) {
+        URL[] urLs = Launcher.getBootstrapClassPath().getURLs();
+        Arrays.stream(urLs).forEach(System.out::println);
+
+        // 从上面的路劲中随便挑选一个类, 查看其类加载器 :
+        System.out.println("===========extension class loader spilt line===========");
+        String extensionUrl = System.getProperty(EXT_URL);
+        Arrays.stream(extensionUrl.split(SEMICOLON)).forEach(System.out::println);
+
+    }
+
+}
