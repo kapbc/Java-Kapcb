76b7b159395ffe5156051eb333ab430874d1d250
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-01-25 14:03:51 +0800

    re factory

---
+++ src/main/java/com/kapcb/ccc/common/IEnum.java
@@ -0,0 +1,22 @@
+package com.kapcb.ccc.common;
+
+/**
+ * <a>Title: IEnum </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: IEnum <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/1/25 14:01
+ * @since 1.0
+ */
+public interface IEnum<T> {
+
+    /**
+     * get value
+     *
+     * @return T
+     */
+    T value();
+
+}
---
+++ src/main/java/com/kapcb/ccc/common/StringEnumPool.java
@@ -0,0 +1,23 @@
+package com.kapcb.ccc.common;
+
+/**
+ * <a>Title: StringEnumPool </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: StringEnumPool <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/1/25 14:02
+ * @since 1.0
+ */
+public enum StringEnumPool implements IEnum<String> {
+
+
+
+    ;
+
+    @Override
+    public String value() {
+        return null;
+    }
+}
