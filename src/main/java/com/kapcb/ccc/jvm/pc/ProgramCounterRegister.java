583da78b392a70c31c08a276f6d00931e1eea828
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-01-26 18:22:01 +0800

    re factory

---
+++ src/main/java/com/kapcb/ccc/jvm/pc/ProgramCounterRegister.java
@@ -0,0 +1,25 @@
+package com.kapcb.ccc.jvm.pc;
+
+/**
+ * <a>Title: ProgramCounterRegister </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: ProgramCounterRegister <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/1/26 18:21
+ * @since 1.0
+ */
+public class ProgramCounterRegister {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static int minus() {
+        int i = 3;
+        int j = 4;
+        return j - i;
+    }
+
+}
