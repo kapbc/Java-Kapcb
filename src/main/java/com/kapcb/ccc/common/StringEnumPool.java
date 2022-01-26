35590216b2d709622609871a518fc2546e72de7b
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-01-26 17:21:08 +0800

    add enums

--- src/main/java/com/kapcb/ccc/common/StringEnumPool.java
+++ src/main/java/com/kapcb/ccc/common/StringEnumPool.java
@@ -11,6 +11,19 @@ package com.kapcb.ccc.common;
  * @since 1.0
  */
 public enum StringEnumPool implements IEnum<String> {
+
+    /**
+     * Static Resources IO Path
+     */
+    MENU_TXT_RESOURCES_PATH("src/main/resource/txt/menu.txt", "Stream Menu Resources Test Txt File"),
+    CITY_TXT_RESOURCES_PATH("src/main/resource/txt/cities.txt", "Stream City Test Resources Txt File"),
+    EMAIL_HANDLER_RESOURCES_PATH("src/main/resource/txt/score.txt", "Email Handler Data Test Txt File"),
+    ALICE_TXT_RESOURCES_PATH("src/main/resource/txt/alice30.txt", "Stream PrimitiveTypeStreams Test Txt File"),
+    PROGRAM_PROPERTIES_RESOURCES_PATH("src/main/resource/program.properties", "Program Test Resources Location"),
+    START_UP_PROPERTIES_RESOURCES_PATH("src/main/resource/start.properties", "Start Up Properties Resources Location"),
+    STREAM_TEST_TXT_RESOURCES_PATH("src/main/resource/txt/streamTest.txt", "Stream Optional Test Static Resources Location"),
+    LOG_MESSAGE_PROPERTIES_RESOURCES_PATH("src/main/resource/logmessage_en.properties", "The Log Message Properties Resources Path"),
+
     ;
 
     private String value;
