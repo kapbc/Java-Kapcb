31fc6a2008eda82ce0191ef8a26110fa35d410ab
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-01-27 16:40:37 +0800

    add slot test

---
+++ src/main/java/com/kapcb/ccc/jvm/lv/SlotTest.java
@@ -0,0 +1,14 @@
+package com.kapcb.ccc.jvm.lv;
+
+/**
+ * <a>Title: SlotTest </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: SlotTest <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/1/27 16:40
+ * @since 1.0
+ */
+public class SlotTest {
+}
