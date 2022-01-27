2460b67c1c71fb1ec4d2304725dd8c27c613ac8e
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-01-27 16:41:57 +0800

    add slot test

--- src/main/java/com/kapcb/ccc/jvm/lv/SlotTest.java
+++ src/main/java/com/kapcb/ccc/jvm/lv/SlotTest.java
@@ -11,4 +11,21 @@ package com.kapcb.ccc.jvm.lv;
  * @since 1.0
  */
 public class SlotTest {
+
+    public void localVar() {
+        int a = 0;
+        System.out.println(a);
+        int b = 0;
+    }
+
+    public void localVar1() {
+        {
+            int a = 0;
+            System.out.println(a);
+        }
+
+        int b = 0;
+    }
+
+
 }
