8ca4b50dfeea5f9b31e0a7752f036d013ec972a6
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-01-27 20:22:03 +0800

    add father class

---
+++ src/main/java/com/kapcb/ccc/jvm/dl/Father.java
@@ -0,0 +1,23 @@
+package com.kapcb.ccc.jvm.dl;
+
+/**
+ * <a>Title: Father </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: Father <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/1/27 20:19
+ * @since 1.0
+ */
+public class Father {
+
+    public void eat(String food) {
+        System.out.println("transfer father's eat method call : " + food);
+    }
+
+    public void say(String keyword) {
+        System.out.println("transfer father's say method call : " + keyword);
+    }
+
+}
