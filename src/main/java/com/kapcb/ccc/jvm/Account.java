01ca93fec52fc2790cfdf42f4347e39033a13b05
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-02-02 15:03:56 +0800

    add Customer

---
+++ src/main/java/com/kapcb/ccc/jvm/Account.java
@@ -0,0 +1,15 @@
+package com.kapcb.ccc.jvm;
+
+/**
+ * <a>Title: Account </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: Account <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/2/2 14:59
+ * @since 1.0
+ */
+public class Account {
+
+}
---
+++ src/main/java/com/kapcb/ccc/jvm/Customer.java
@@ -0,0 +1,29 @@
+package com.kapcb.ccc.jvm;
+
+/**
+ * <a>Title: Customer </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: Customer <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/2/2 14:57
+ * @since 1.0
+ */
+public class Customer {
+
+    private Integer id = 100001;
+
+    private String name;
+
+    private Account account;
+
+    {
+        name = "kapcd";
+    }
+
+    public Customer() {
+        account = new Account();
+    }
+
+}
