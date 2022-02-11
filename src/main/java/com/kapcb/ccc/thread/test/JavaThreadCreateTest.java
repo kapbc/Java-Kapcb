5983c22e098dd044fc455fa16a229a8b83dbc60d
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-02-11 20:39:38 +0800

    add java thread create test

---
+++ src/main/java/com/kapcb/ccc/thread/test/JavaThreadCreateTest.java
@@ -0,0 +1,20 @@
+package com.kapcb.ccc.thread.test;
+
+/**
+ * <a>Title: JavaThreadCreateTest </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: JavaThreadCreateTest <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/2/11 20:39
+ * @since 1.0
+ */
+public class JavaThreadCreateTest {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
