c3d35886abd657d273c2efc553f8cdfc8cd0a166
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-02-11 20:44:18 +0800

    add thread create test

--- src/main/java/com/kapcb/ccc/thread/test/JavaThreadCreateTest.java
+++ src/main/java/com/kapcb/ccc/thread/test/JavaThreadCreateTest.java
@@ -1,5 +1,12 @@
 package com.kapcb.ccc.thread.test;
 
+import com.kapcb.ccc.thread.create.MyCallable;
+import com.kapcb.ccc.thread.create.MyRunnable;
+import com.kapcb.ccc.thread.create.MyThread;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 /**
  * <a>Title: JavaThreadCreateTest </a>
  * <a>Author: Kapcb <a>
@@ -13,6 +20,27 @@ package com.kapcb.ccc.thread.test;
 public class JavaThreadCreateTest {
 
     public static void main(String[] args) {
+        // 线程的第一种常见方式
+        MyThread myThread = new MyThread();
+        myThread.start();
+
+        // 线程的第二种创建方式
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread = new Thread(myRunnable);
+        thread.start();
+
+        // 线程的第三种创建方式
+        MyCallable myCallable = new MyCallable();
+        FutureTask<String> futureTask = new FutureTask<>(myCallable);
+        Thread futureTaskThread = new Thread(futureTask);
+        futureTaskThread.start();
+        String callableResult = null;
+        try {
+            callableResult = futureTask.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        System.out.println(callableResult);
 
 
     }
