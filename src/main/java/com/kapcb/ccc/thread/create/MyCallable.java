1785a962cb6e7a7c61e4994e4886aa86c1d2c410
eircccallroot@yeah.net <eircccallroot@yeah.net>
2022-02-11 20:37:51 +0800

    add callable

---
+++ src/main/java/com/kapcb/ccc/thread/create/MyCallable.java
@@ -0,0 +1,22 @@
+package com.kapcb.ccc.thread.create;
+
+import java.util.concurrent.Callable;
+
+/**
+ * <a>Title: MyCallable </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: MyCallable <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/2/11 20:35
+ * @since 1.0
+ */
+public class MyCallable implements Callable<String> {
+
+    @Override
+    public String call() throws Exception {
+        return "current thread is : " + Thread.currentThread();
+    }
+
+}
