643b4a5b22d97b7c4d1378d84ebabc8a124b9ba2
kapcb <eircccallroot@yeah.net>
2022-09-15 00:04:34 +0800

    add ThreeThreadPrint

---
+++ src/main/java/com/kapcb/ccc/thread/output/ThreeThreadPrint.java
@@ -0,0 +1,26 @@
+package com.kapcb.ccc.thread.output;
+
+/**
+ * <a>Title: ThreeThreadPrint </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: ThreeThreadPrint <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/9/15 12:03 AM
+ * @since 1.0
+ */
+public class ThreeThreadPrint {
+
+    /**
+     * 三个线程交替打印输出ABC
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
