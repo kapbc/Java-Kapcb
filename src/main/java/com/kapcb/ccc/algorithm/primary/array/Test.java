d146d6a90db918def0bf9c458411d935bdf43a53
kapcb <eircccallroot@yeah.net>
2022-09-18 11:42:59 +0800

    Create Test.java

---
+++ src/main/java/com/kapcb/ccc/algorithm/primary/array/Test.java
@@ -0,0 +1,28 @@
+package com.kapcb.ccc.algorithm.primary.array;
+
+/**
+ * <a>Title: Test </a>
+ * <a>Description: Test <a>
+ *
+ * @version 1.0
+ * @date 2022/9/12 10:36
+ * @since 1.0
+ */
+public class Test {
+
+    /**
+     * 腾讯面试：
+     * 给定整数power，给定一个数组arr，给定一个数组reverse。含义如下：
+     * arr数组的长度一定是2的power次方，reverse每个值一定都在0~power范围。
+     * 例如power=2，arr={3,1,2,4},reverse={0,1,0,2}
+     * 任何一个在前的数字可以和任何一个在后的数组，构成一对数。可能是升序关系、相等关系或者降序关系。
+     * 比如arr开始时有如下的降序对：(3,1)、(3,2)、(4,2)，一共3个
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+
+}
