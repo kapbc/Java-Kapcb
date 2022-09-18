424f79508b819662890647683c0bc501297f55de
kapcb <eircccallroot@yeah.net>
2022-09-18 11:40:51 +0800

    Create aaa.java

---
+++ src/main/java/com/kapcb/ccc/algorithm/primary/array/aaa.java
@@ -0,0 +1,31 @@
+package com.kapcb.ccc.algorithm.primary.array;
+
+/**
+ * <a>Title: aaa </a>
+ * <a>Description: aaa <a>
+ *
+ * @version 1.0
+ * @date 2022/9/12 12:39
+ * @since 1.0
+ */
+public class aaa {
+
+    /**
+     * 京东面试：
+     * 整型数组arr长度为n(3<=n<=10^4)
+     * 最初每个数字是<=200的正数且满足如下条件：
+     * 1、arr[0] <= arr[1]
+     * 2、arr[n-1] <= arr[n-2]
+     * 3、arr[i] <= max(arr[i-1],arr[i+1])
+     * 但是现在arr有些数字丢失了，比如k位置的数字之前是正数，丢失之后k位置的数为0
+     * 计算可能有多少种不同的arr可以满足以上条件
+     * 比如[6,0,9]只还原成[6,9,9]满足全部三个条件，所以返回1种
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+
+}
