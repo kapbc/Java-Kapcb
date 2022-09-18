46fb5f60bcb25a4541bd802a228f17b7d28fd659
kapcb <eircccallroot@yeah.net>
2022-09-19 00:06:04 +0800

    Add ReverseDoubleList

--- src/main/java/com/kapcb/ccc/algorithm/primary/linkedlist/ReverseDoubleList.java
+++ src/main/java/com/kapcb/ccc/algorithm/primary/linkedlist/ReverseDoubleList.java
@@ -12,6 +12,39 @@ package com.kapcb.ccc.algorithm.primary.linkedlist;
 public class ReverseDoubleList {
 
     public static void main(String[] args) {
+        ListNode one = new ListNode(1, null, null);
+        ListNode two = new ListNode(2, null, one);
+        ListNode three = new ListNode(3, null, two);
+        ListNode four = new ListNode(4, null, three);
+        ListNode five = new ListNode(5, null, four);
+        one.next = two;
+        two.next = three;
+        three.next = four;
+        four.next = five;
+        ListNode reverse = reverse(one);
+        System.out.println("reverse = " + reverse);
+    }
+
+    private static ListNode reverse(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode cur = head;
+        ListNode prev = null;
+
+        while (cur != null) {
+
+            ListNode next = cur.next;
+            cur.next = prev;
+            cur.prev = next;
+            prev = cur;
+            cur = next;
+        }
+
+        return prev;
+
 
     }
 
---
+++ src/main/java/com/kapcb/ccc/algorithm/primary/linkedlist/ReverseLinkedListOfSpecifiedRange.java
@@ -0,0 +1,20 @@
+package com.kapcb.ccc.algorithm.primary.linkedlist;
+
+/**
+ * <a>Title: ReverseLinkedListOfSpecifiedRange </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: ReverseLinkedListOfSpecifiedRange <a>
+ * <a href="https://www.nowcoder.com/practice/b58434e200a648c589ca2063f1faf58c?tpId=295&tqId=654&ru=/exam/oj&qru=/ta/format-top101/question-ranking&sourceUrl=%2Fexam%2Foj">...</a>
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/9/18 11:34 PM
+ * @since 1.0
+ */
+public class ReverseLinkedListOfSpecifiedRange {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
