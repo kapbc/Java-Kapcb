df7e624d17f4c830d8514b170ae169af0733a701
kapcb <eircccallroot@yeah.net>
2022-09-18 11:47:53 +0800

    Create RandomNumThree.java

---
+++ src/main/java/com/kapcb/ccc/algorithm/primary/array/RandomNumThree.java
@@ -0,0 +1,19 @@
+package com.kapcb.ccc.algorithm.primary.probability;
+
+/**
+ * <a>Title: RandomNumThree </a>
+ * <a>Description: RandomNumThree <a>
+ *
+ * @version 1.0
+ * @date 2022/8/28 22:50
+ * @since 1.0
+ */
+public class RandomNumThree {
+
+    // a ~ b等概率实现c ~ d等概率
+    public static void main(String[] args) {
+
+    }
+
+
+}
