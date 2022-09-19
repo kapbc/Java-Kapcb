cbe113c978a242b455c8e381f8015cc53e8e016e
kapcb <eircccallroot@yeah.net>
2022-09-19 23:01:58 +0800

    Add ReverseLinkedListOfKGroup

---
+++ src/main/java/com/kapcb/ccc/algorithm/primary/linkedlist/ReverseLinkedListOfKGroup.java
@@ -0,0 +1,25 @@
+package com.kapcb.ccc.algorithm.primary.linkedlist;
+
+/**
+ * <a>Title: ReverseLinkedListOfKGroup </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: ReverseLinkedListOfKGroup <a>
+ * <a href="https://www.nowcoder.com/practice/b49c3dc907814e9bbfa8437c251b028e?tpId=295&tqId=722&ru=/exam/oj&qru=/ta/format-top101/question-ranking&sourceUrl=%2Fexam%2Foj">...</a>
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/9/19 10:58 PM
+ * @since 1.0
+ */
+public class ReverseLinkedListOfKGroup {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static ListNode reverseKGroup(ListNode head, int k) {
+        // write code here
+
+
+
+    }
+}
