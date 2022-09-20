22f4cc6759dd31039ccf38e3a35e3ce3895a13ce
kapcb <eircccallroot@yeah.net>
2022-09-20 22:36:57 +0800

    add ParityRearrangementOfLinkedList

---
+++ src/main/java/com/kapcb/ccc/algorithm/primary/linkedlist/ParityRearrangementOfLinkedList.java
@@ -0,0 +1,14 @@
+package com.kapcb.ccc.algorithm.primary.linkedlist;
+
+/**
+ * <a>Title: ParityRearrangementOfLinkedList </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: 链表的奇偶重排 <a>
+ * <a href="https://www.nowcoder.com/practice/02bf49ea45cd486daa031614f9bd6fc3?tpId=295&tqId=1073463&ru=/exam/oj&qru=/ta/format-top101/question-ranking&sourceUrl=%2Fexam%2Foj">...</a>
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/9/20 10:35 PM
+ * @since 1.0
+ */
+public class ParityRearrangementOfLinkedList {
+}
