05fd1c98d92b700b800bb00c28e470d306527ba0
kapcb <eircccallroot@yeah.net>
2022-09-21 22:39:34 +0800

    add ThreeNumSum

---
+++ src/main/java/com/kapcb/ccc/algorithm/primary/array/ThreeNumSum.java
@@ -0,0 +1,26 @@
+package com.kapcb.ccc.algorithm.primary.array;
+
+import java.util.List;
+
+/**
+ * <a>Title: ThreeNumSum </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: ThreeNumSum <a>
+ * <a href="https://leetcode.cn/problems/3sum/">...</a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/9/21 10:37 PM
+ * @since 1.0
+ */
+public class ThreeNumSum {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+    }
+
+}
