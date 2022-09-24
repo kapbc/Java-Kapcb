b788b457013317ef17f753b9da47ac9778a38f0a
kapcb <eircccallroot@yeah.net>
2022-09-24 15:08:48 +0800

    add authot

---
+++ src/main/java/com/kapcb/ccc/algorithm/primary/str/LongestPalindromicSubstring.java
@@ -0,0 +1,24 @@
+package com.kapcb.ccc.algorithm.primary.str;
+
+/**
+ * <a>Title: LongestPalindromicSubstring </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: 最长回文字串 <a>
+ * <a href="https://leetcode.cn/problems/longest-palindromic-substring/">...</a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/9/24 3:07 PM
+ * @since 1.0
+ */
+public class LongestPalindromicSubstring {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static String longestPalindrome(String s) {
+
+    }
+
+}
