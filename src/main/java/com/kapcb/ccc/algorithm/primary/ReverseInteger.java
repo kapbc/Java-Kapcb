df4fa7c11cccf564d76823b48f9033dca691cb58
kapcb <eircccallroot@yeah.net>
2022-09-25 13:11:22 +0800

    add ReverseInteger

--- src/main/java/com/kapcb/ccc/algorithm/primary/ReverseInteger.java
+++ src/main/java/com/kapcb/ccc/algorithm/primary/ReverseInteger.java
@@ -14,13 +14,20 @@ package com.kapcb.ccc.algorithm.primary;
 public class ReverseInteger {
 
     public static void main(String[] args) {
-
+        int reverse = reverse(-1200);
+        System.out.println("reverse = " + reverse);
 
     }
 
     private static int reverse(int x) {
 
-        return 0;
+        long n = 0;
+        while (x != 0) {
+            n = n * 10 + x % 10;
+            x = x / 10;
+        }
+
+        return (int) n == n ? (int) n : 0;
     }
 
 }
