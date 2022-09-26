bc47e739a8aeeebf426d3984e3175662fe0ffa26
kapcb <2530628867@qq.com>
2022-09-26 23:02:00 +0800

    Add ReverseStack

--- src/main/java/com/kapcb/ccc/algorithm/book/stack/ReverseStack.java
+++ src/main/java/com/kapcb/ccc/algorithm/book/stack/ReverseStack.java
@@ -41,7 +41,15 @@ public class ReverseStack {
     }
 
     public static void main(String[] args) {
+        int[] arr = {2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19};
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            stack.push(arr[i]);
+        }
 
+        System.out.println("stack = " + stack);
+        reverse(stack);
+        System.out.println("stack = " + stack);
     }
 
 }
