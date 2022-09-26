6ef22b8e0a37ff7a7b20f8a3cb4d990ff44082cb
kapcb <2530628867@qq.com>
2022-09-26 22:00:08 +0800

    add PalindromeNumber

---
+++ src/main/java/com/kapcb/ccc/algorithm/primary/num/PalindromeNumber.java
@@ -0,0 +1,14 @@
+package com.kapcb.ccc.algorithm.primary.num;
+
+/**
+ * <a>Title: PalindromeNumber </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: PalindromeNumber <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/9/26 21:59
+ * @since 1.0
+ */
+public class PalindromeNumber {
+}
