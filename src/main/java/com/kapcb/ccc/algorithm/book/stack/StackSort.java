00c24f8eb0f603229296f2fd6017da291719b2a7
kapcb <2530628867@qq.com>
2022-09-26 23:04:47 +0800

    add StackSort

---
+++ src/main/java/com/kapcb/ccc/algorithm/book/stack/StackSort.java
@@ -0,0 +1,25 @@
+package com.kapcb.ccc.algorithm.book.stack;
+
+import java.util.Stack;
+
+/**
+ * <a>Title: StackSort </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: 使用栈实现另一个栈的排序 <a>
+ *
+ * @author Kapcb
+ * @version 1.0
+ * @date 2022/9/26 23:03
+ * @since 1.0
+ */
+public class StackSort {
+
+    private static void sort(Stack<Integer> stack) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
